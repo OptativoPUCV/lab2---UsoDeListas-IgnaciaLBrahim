eed120e71be28672a8679e97d8986b91b9364454
Your Name <you@example.com>
2024-03-21 20:54:03 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -128,7 +128,7 @@ int parentesisBalanceados(char *cadena) {
         }
       else
       {
-        if((char) top(pila) == '(' && *elemento != ')' || (char) top(pila) == '[' && *elemento != ']' || (char) top(pila) == '{' && *elemento != '}' || top(pila) == NULL)
+        if((top(pila) == '(' && *elemento != ')') ||(top(pila) == '[' && *elemento != ']') || (top(pila) == '{' && *elemento != '}') || (top(pila) == NULL))
         {
           return 0;
         }
